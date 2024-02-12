920964c5de27bbb95b261991a2a286416c9cdb5e
aonnml <radchanon694@gmail.com>
2024-02-13 01:02:10 +0700

    lab18_2.cpp completed

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,8 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect R,Rect S){
+    double x = max(0.0, min(R.x + R.w, S.x + S.w) - max(R.x,S.x));
+    double y = max(0.0, min(R.y,S.y) - max(R.y - R.h, S.y - S.h));
+    return x * y;
 }
